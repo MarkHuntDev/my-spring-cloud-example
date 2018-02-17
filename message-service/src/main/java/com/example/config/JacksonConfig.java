fd6b16bdac8f6336aa5084f63943f46502f2e588
Rinat Zalyaletdinov <rinat_zalyaletdinov@epam.com>
2018-02-17 23:12:43 +0300

    Fix integration test with Hoverfly

---
+++ message-service/src/main/java/com/example/config/JacksonConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        return staticMappingJackson2HttpMessageConverter();
+    }
+
+    public static MappingJackson2HttpMessageConverter staticMappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+}
--- message-service/src/test/java/com/example/controller/MessageControllerIT.java
+++ message-service/src/test/java/com/example/controller/MessageControllerIT.java
@@ -4,6 +4,9 @@ import static io.specto.hoverfly.junit.core.SimulationSource.dsl;
 import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
 import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
 
+import com.example.config.JacksonConfig;
+import com.example.model.Celebrity;
+import com.example.model.Greeting;
 import com.example.model.Message;
 import io.specto.hoverfly.junit.rule.HoverflyRule;
 import org.junit.Assert;
@@ -12,8 +15,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,11 +30,16 @@ public class MessageControllerIT {
 
     @ClassRule
     public static HoverflyRule hoverflyRule = HoverflyRule
-            .inSimulationMode(
-                    dsl(service("localhost:8500")
-                            .get("/messages?greetingId=1&celebrityId=1")
+            .inSimulationMode(dsl(
+                    service("greeting-service:8080")
+                            .get("/greetings/1")
                             .willReturn(
-                                    success("{\"value\":\"Good afternoon, Ornella Muti\"}", "application/json"))));
+                                    success(toJsonString(new Greeting().setValue("Good afternoon")), "application/json")),
+                    service("celebrity-service:8080")
+                            .get("/celebrities/1")
+                            .willReturn(
+                                    success(toJsonString(new Celebrity().setName("Ornella Muti")), "application/json"))
+            )).printSimulationData();
 
     @Autowired
     private MessageController controller;
@@ -34,6 +47,18 @@ public class MessageControllerIT {
     @Test
     public void testMessage() {
         Message message = controller.message(1, 1);
+        System.out.println(message.getValue());
         Assert.assertEquals("Good afternoon, Ornella Muti", message.getValue());
+    }
+
+    private static String toJsonString(Object object) {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        try {
+            JacksonConfig.staticMappingJackson2HttpMessageConverter().write(object, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        } catch (IOException e) {
+            // todo:
+            e.printStackTrace();
+        }
+        return mockHttpOutputMessage.getBodyAsString();
     }
 }
