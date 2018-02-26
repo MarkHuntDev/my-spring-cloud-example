edbdc662089a4395bf9b3c3626ed278e332cecd7
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-02-26 17:26:37 +0300

    Add TestTokenEnhancer to authorization-server

--- authorization-server/src/main/java/com/example/config/AuthorizationServerConfig.java
+++ authorization-server/src/main/java/com/example/config/AuthorizationServerConfig.java
@@ -5,13 +5,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableAuthorizationServer
@@ -49,11 +58,30 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints
           .tokenStore(tokenStore())
           .authenticationManager(this.authenticationManager)
-          .accessTokenConverter(accessTokenConverter());
+          .accessTokenConverter(accessTokenConverter())
+          .tokenEnhancer(tokenEnhancerChain());
     } // @formatter:on
 
     @Autowired
     public void setAuthenticationManager(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
+    }
+
+    private TokenEnhancerChain tokenEnhancerChain() {
+        TokenEnhancerChain chain = new TokenEnhancerChain();
+        chain.setTokenEnhancers(Arrays.asList(accessTokenConverter(), new TestTokenEnhancer()));
+        return chain;
+    }
+
+    private static class TestTokenEnhancer implements TokenEnhancer {
+
+        @Override
+        public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+            Map<String, Object> additionalJwtProperties = new HashMap<>();
+            additionalJwtProperties.put("testProperty", "testValue");
+
+            ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalJwtProperties);
+            return accessToken;
+        }
     }
 }
