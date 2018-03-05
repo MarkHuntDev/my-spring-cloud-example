055c85f3ade6e4d13a1ba938d0f750cc33fa4dba
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-03-05 17:08:28 +0300

    Remove redundant import

--- message-service/src/test/java/com/example/controller/MessageControllerIT.java
+++ message-service/src/test/java/com/example/controller/MessageControllerIT.java
@@ -16,7 +16,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
