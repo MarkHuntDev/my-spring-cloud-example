f9fef89b5e9c4641da8b483c9825265efce4a021
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-03-05 17:11:14 +0300

    Refactoring

--- celebrity-service/src/main/java/com/example/config/ResourceServerConfiguration.java
+++ celebrity-service/src/main/java/com/example/config/ResourceServerConfiguration.java
@@ -1,32 +1,13 @@
 package com.example.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+// todo: introduce profile that switches on/off security
 @Configuration
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
-
-    @Value("${signing-key:test123}")
-    private String signingKey;
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signingKey);
-        return converter;
-    }
-
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
 }
---
+++ celebrity-service/src/main/java/com/example/config/oauth2/TokenConfig.java
@@ -0,0 +1,28 @@
+package com.example.config.oauth2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class TokenConfig {
+
+    // todo: need to use asymmetric key
+    @Value("${signing-key:test123}")
+    private String signingKey;
+
+    @Bean
+    JwtAccessTokenConverter accessTokenConverter() {
+        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signingKey);
+        return converter;
+    }
+
+    @Bean
+    TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+}
