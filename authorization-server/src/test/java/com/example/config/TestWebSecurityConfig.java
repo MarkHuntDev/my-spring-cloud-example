ceb8685d3eddc72c97dbef63cc5644ee2cb7c08a
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2018-09-24 14:53:31 +0300

    Fix WebSecurityConfig

--- authorization-server/src/test/java/com/example/config/TestWebSecurityConfig.java
+++ authorization-server/src/test/java/com/example/config/TestWebSecurityConfig.java
@@ -2,13 +2,14 @@ package com.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Profile("test")
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class TestWebSecurityConfig /*extends WebSecurityConfigurerAdapter */ {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off
